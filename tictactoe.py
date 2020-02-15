b4b605b3b8969f3bcb4aa5181e3d1fab43c7b6fe
Robert Alford <robertsalford@gmail.com>
2020-02-14 16:54:42 -0800

    rearrange code for readability and use built in funcs

--- tictactoe.py
+++ tictactoe.py
@@ -1,41 +1,17 @@
-import itertools
+from itertools import chain
 
-SPACES_ON_BOARD = (1,2,3,4,5,6,7,8,9)  # todo: use range
+SPACES_ON_BOARD = range(1, 10)
 
 WINNING_COMBOS = (
-    (1,2,3),
-    (4,5,6),
-    (7,8,9),
-    (1,4,7),
-    (2,5,8),
-    (3,6,9),
-    (1,5,9),
-    (3,5,7)
+    (1, 2, 3),
+    (4, 5, 6),
+    (7, 8, 9),
+    (1, 4, 7),
+    (2, 5, 8),
+    (3, 6, 9),
+    (1, 5, 9),
+    (3, 5, 7)
 )
-
-# tie
-# players_moves = {
-#     'x': [5,7,2,4,9],
-#     'o': [1,3,8,6],
-# }
-
-players_moves = {
-    'x': [],
-    'o': [],
-}
-
-
-def check_for_winner(players_moves):
-    all_moves = [move for moves in players_moves.values() for move in moves]  # could use itertools chain
-    if all(move in all_moves for move in SPACES_ON_BOARD):
-        return "It's a tie!"
-    for player, moves in players_moves.items():
-        for combo in WINNING_COMBOS:
-            if all(move in moves for move in combo):
-                return player + ' wins'
-
-
-print(check_for_winner(players_moves))
 
 board = " 1 | 2 | 3 \n ---------- \n 4 | 5 | 6 \n ---------- \n 7 | 8 | 9"
 # print(board)
@@ -46,6 +22,11 @@ Enter a number from 1-9 to place your move on the board in the corresponding spa
 
 {}
 """.format(board)
+
+players_moves = {
+    'x': [],
+    'o': [],
+}
 
 
 def draw_current_board(board, players_moves):
@@ -59,7 +40,15 @@ def draw_current_board(board, players_moves):
 
     return board
 
-# print(draw_current_board(board, players_moves))
+
+def check_for_winner(players_moves):
+    all_moves = list(chain.from_iterable(players_moves.values()))  # could use itertools chain
+    if all(move in all_moves for move in SPACES_ON_BOARD):
+        return "It's a tie!"
+    for player, moves in players_moves.items():
+        for combo in WINNING_COMBOS:
+            if all(move in moves for move in combo):
+                return player + ' wins'
 
 
 if __name__ == "__main__":
@@ -77,6 +66,7 @@ if __name__ == "__main__":
             move = input("Player {}, enter your move.\n".format(player))
             moves.append(int(move))
             print(players_moves)
+
             if check_for_winner(players_moves):
                 print(check_for_winner(players_moves))
                 playing_game = False
